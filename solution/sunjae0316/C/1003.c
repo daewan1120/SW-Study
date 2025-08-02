e5eca83cec03cc2d3be6e93d70482a012526f1af
sunjae0316 <kimsunjae0316@gmail.com>
2025-08-02 14:01:24 +0900

    feat: Add week1 problem solution in C

---
+++ solution/sunjae0316/C/1003.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	int age = 20;
+	float height = 123.4;
+	char blood = 'A';
+	
+	printf("나이: %d\n",age);
+	printf("키: %.1lf\n",height);
+	printf("혈액형: %c\n",blood);
+	return 0;
+}
